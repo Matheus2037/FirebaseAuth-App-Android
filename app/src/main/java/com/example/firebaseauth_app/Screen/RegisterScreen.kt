16852fb70844d6530361aff7be5f85560bc8d4a3
unknown <matheusdasilvagastaldi@gmail.com>
2025-04-25 12:32:17 -0300

    Tela de registro

---
+++ app/src/main/java/com/example/firebaseauth_app/Screen/RegisterScreen.kt
@@ -0,0 +1,89 @@
+package com.example.firebaseauth_app.Screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.auth.FirebaseAuth
+
+@Composable
+fun RegisterScreen(
+    onRegisterSuccess: () -> Unit,
+    onNavigateToLogin: () -> Unit
+){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordConfirmation by remember { mutableStateOf("") }
+    var error by remember { mutableStateOf<String?>(null) }
+
+    Column(
+        Modifier.padding(16.dp)
+    ) {
+        Text(text = "Registro", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        OutlinedTextField(
+            value = passwordConfirmation,
+            onValueChange = { passwordConfirmation = it },
+            label = { Text("Confirm Password") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        error?.let {
+            Text(text = it, color = Color.Red)
+        }
+
+        Button(onClick ={
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if(password == passwordConfirmation) {
+                        if (task.isSuccessful) {
+                            onRegisterSuccess()
+                        } else {
+                            error = task.exception?.message
+                        }
+                    }else{
+                        error = task.exception?.message
+                    }
+                }
+        }) {
+            Text("Entrar")
+        }
+
+        TextButton(onClick = onNavigateToLogin) {
+            Text("Criar conta")
+        }
+    }
+}
