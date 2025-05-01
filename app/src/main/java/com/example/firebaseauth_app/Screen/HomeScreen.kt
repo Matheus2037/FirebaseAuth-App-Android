61b4ced10dc7d0fc6047724408dc8b453e56ee03
Matheus <matheusdasilvagastaldi@gmail.com>
2025-05-01 20:40:51 -0300

    Centralizando elementos Home

--- app/src/main/java/com/example/firebaseauth_app/Screen/HomeScreen.kt
+++ app/src/main/java/com/example/firebaseauth_app/Screen/HomeScreen.kt
@@ -1,23 +1,36 @@
 package com.example.firebaseauth_app.Screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
 
 
 @Composable
 fun HomeScreen(navController: NavController){
-    Column(Modifier.padding(16.dp)){
+    Column(
+        Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
 
-        Text(text = "Login realizado com sucesso")
+        Text(text = "Login realizado com sucesso", fontSize = 24.sp)
 
-        Button(onClick = {
+
+        Button(
+            modifier = Modifier.padding(16.dp),
+            onClick = {
             FirebaseAuth.getInstance().signOut()
             navController.navigate("login"){
                 popUpTo("home") { inclusive = true }
@@ -26,4 +39,10 @@ fun HomeScreen(navController: NavController){
             Text(text = "Voltar")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(navController = NavController(LocalContext.current))
 }
